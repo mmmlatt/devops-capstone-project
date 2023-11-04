ef18b234069efb600fbd57873cb7ef7ede72c699
mmmlatt <mikael.lattunen@hotmail.fi>
2023-11-04 03:41:05 -0400

    created read_an_account route

--- service/routes.py
+++ service/routes.py
@@ -69,7 +69,13 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
-
+@app.route("/accounts/<int:id>", methods=["GET"])
+def read_account(id):
+    app.logger.info(f"Request to read an Account with id: {id}")
+    account = Account.find(id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id {id} could not be found.") 
+    return(account.serialize(), status.HTTP_200_OK)
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
